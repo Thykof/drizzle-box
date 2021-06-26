94577ccdb4e16412ad938f705b48986d217921ab
Nathan SEVA <nathan.seva@niji.fr>
2021-06-26 17:28:23 +0200

    Remove other contracts in app

--- app/src/drizzleOptions.js
+++ app/src/drizzleOptions.js
@@ -1,7 +1,5 @@
 import Web3 from "web3";
-import ComplexStorage from "./contracts/ComplexStorage.json";
 import SimpleStorage from "./contracts/SimpleStorage.json";
-import TutorialToken from "./contracts/TutorialToken.json";
 
 const options = {
   web3: {
@@ -9,7 +7,7 @@ const options = {
     customProvider: new Web3(Web3.givenProvider), // connect with MetaMask
     // customProvider: new Web3("ws://localhost:8545"), // connect directly to ganache
   },
-  contracts: [SimpleStorage, ComplexStorage, TutorialToken],
+  contracts: [SimpleStorage],
 };
 
 export default options;
