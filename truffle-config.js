4b64e424cb4e2f0fd9c2a51892871c70c66692b3
Nathan SEVA <nathan.seva@niji.fr>
2021-06-26 17:43:46 +0200

    Add ropsten in truffle-config

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,9 @@
 const path = require("path");
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+
+const URL_ROPSTEN = 'https://ropsten.infura.io/v3/c365267ea69443cf9af909a295e1b8b5';
+
+const MNEMONIC = 'spoil shiver salon alert paper robust budget recall stick possible jaguar liar';
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -9,6 +14,14 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*"
+    },
+    ropsten: {
+      provider: function () {
+        return new HDWalletProvider(MNEMONIC, URL_ROPSTEN)
+      },
+      network_id: 3,
+      skipDryRun: true,
+      networkCheckTimeout: 1e9
     }
   }
 };
